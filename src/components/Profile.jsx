0354bb1053b2c374595bec57ce8f4f47917a520f
Mihail Ogarkov <medvedrulit2021@gmail.com>
2023-10-12 16:49:07 +0300

    components+grid

---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+const Profile = () => {
+  return (
+    <div className="profile">
+      <div>
+        <img alt="img" src="https://bogatyr.club/uploads/posts/2023-03/1679336570_bogatyr-club-p-zastavka-kosmos-foni-pinterest-9.jpg"/>
+      </div>
+      <div>
+        <img alt="ava" src="https://uprostim.com/wp-content/uploads/2021/05/image033-5.jpg"/>
+      </div>
+      <div>
+        Post 1
+      </div>
+      <div>
+        Post 2
+      </div>
+    </div>
+  )
+};
+
+export default Profile;
